4b40404ede51f877526ca0d1456a139d52167d02
Monika <monika.halicka@bristol.ac.uk>
2026-01-30 18:55:13 +0000

    updated run all file

--- run-all.sh
+++ run-all.sh
@@ -11,8 +11,9 @@ mkdir -p $datadir
 # run all scripts
 cd scripts
 bash download-data.sh $datadir $resultsdir
-Rscript download-pan-cancer-data.r $datadir $resultsdir
+Rscript download-pan-cancer-clinical.r $datadir $resultsdir
 Rscript extract-data.r $datadir $resultsdir
 Rscript clean-clinical.r $datadir $resultsdir
+Rscript predict-proteins.r $datadir $resultsdir
 Rscript combine.r $datadir $resultsdir
 Rscript analysis.r $datadir $resultsdir
